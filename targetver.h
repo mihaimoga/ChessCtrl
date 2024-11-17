6efe3c29976ff7454b5106ebbc12f76f512344c1
Stefan-Mihai MOGA <stefan-mihai@moga.doctor>
2024-11-17 13:03:02 +0200

    Updated copyright information.

--- targetver.h
+++ targetver.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2024 Stefan-Mihai MOGA
+/* Copyright (C) 2024-2025 Stefan-Mihai MOGA
 This file is part of ChessCtrl application developed by Stefan-Mihai MOGA.
 Fully featured Chess Control written in C++ with the help of the MFC library.
 
